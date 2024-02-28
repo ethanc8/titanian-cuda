5bccdee2ec0a209ea0170dd42103ae06586828ce
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 14:24:38 -0600

    Use the newer nvrm

--- make_cuda_dir.sh
+++ make_cuda_dir.sh
@@ -7,13 +7,14 @@ dpkg-deb --raw-extract nvidia-l4t-cuda_34.1.1-20220516211757_arm64.deb build/nvi
 wget https://repo.download.nvidia.com/jetson/t194/pool/main/n/nvidia-l4t-cuda/nvidia-l4t-3d-core_34.1.1-20220516211757_arm64.deb
 dpkg-deb --raw-extract nvidia-l4t-3d-core_34.1.1-20220516211757_arm64.deb build/nvidia-l4t-3d-core
 cp build/nvidia-l4t-cuda/usr/lib/aarch64-linux-gnu/tegra/* build/cuda-11.4/lib
+cp build/nvidia-l4t-cuda/usr/lib/aarch64-linux-gnu/tegra/nvrm* build/cuda-11.4/lib
 rm build/cuda-11.4/lib/libcuda.so
 ln -s libcuda.so.1.1 build/cuda-11.4/lib/libcuda.so
 ln -s libcuda.so.1.1 build/cuda-11.4/lib/libcuda.so.1
-cd nvrm_wrappers
-./build.sh
-cd ..
-cp nvrm_wrappers/build/* build/cuda-11.4/lib
+# cd nvrm_wrappers
+# ./build.sh
+# cd ..
+# cp nvrm_wrappers/build/* build/cuda-11.4/lib
 echo "Ready! Run"
 echo "  export LD_LIBRARY_PATH=\"$(realpath build/cuda-11.4/lib):\$LD_LIBRARY_PATH\""
 echo "to get started."
