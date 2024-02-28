487e7f59220afbd00a85b253f9718339767a1798
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 11:21:28 -0600

    Make the build directory

--- nvrm_wrappers/build.sh
+++ nvrm_wrappers/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p build
 $CC nvrm_gpu_titanian.c -shared -fPIC -o build/nvrm_gpu_titanian.so
 $CC nvrm_host1x.c -shared -lnvrm_graphics -fPIC -o build/nvrm_host1x.so
 $CC nvrm_mem.c -shared -lnvrm -fPIC -o build/nvrm_mem.so
