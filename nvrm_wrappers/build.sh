af3d187ae7e56bc336a8dc07923636c8ef9b6969
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 11:53:29 -0600

    A few fixes

--- nvrm_wrappers/build.sh
+++ nvrm_wrappers/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p build
-$CC nvrm_gpu_titanian.c -shared -fPIC -o build/nvrm_gpu_titanian.so
-$CC nvrm_host1x.c -shared -lnvrm_graphics -L/usr/lib/aarch64-linux-gnu/tegra -fPIC -o build/nvrm_host1x.so
-$CC nvrm_mem.c -shared -lnvrm -fPIC -L/usr/lib/aarch64-linux-gnu/tegra -o build/nvrm_mem.so
-$CC nvrm_sync.c -shared -lnvrm -fPIC -L/usr/lib/aarch64-linux-gnu/tegra -o build/nvrm_sync.so
+$CC nvrm_gpu_titanian.c -shared -fPIC -o build/libnvrm_gpu_titanian.so
+$CC nvrm_host1x.c -shared -lnvrm_graphics -L/usr/lib/aarch64-linux-gnu/tegra -fPIC -o build/libnvrm_host1x.so
+$CC nvrm_mem.c -shared -lnvrm -fPIC -L/usr/lib/aarch64-linux-gnu/tegra -o build/libnvrm_mem.so
+$CC nvrm_sync.c -shared -lnvrm -fPIC -L/usr/lib/aarch64-linux-gnu/tegra -o build/libnvrm_sync.so
--- tests/deviceQueryDrv/build.sh
+++ tests/deviceQueryDrv/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/cuda-10.2/bin/nvcc deviceQueryDrv.cpp -I. -o deviceQueryDrv
+/usr/local/cuda-10.2/bin/nvcc deviceQueryDrv.cpp -lcuda -I. -o deviceQueryDrv
