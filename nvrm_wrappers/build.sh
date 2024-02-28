9142aa06673929e676f5cab01cf3d7a9bd895e17
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 11:23:03 -0600

    Add -L/usr/lib/aarch64-linux-gnu/tegra

--- nvrm_wrappers/build.sh
+++ nvrm_wrappers/build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 $CC nvrm_gpu_titanian.c -shared -fPIC -o build/nvrm_gpu_titanian.so
-$CC nvrm_host1x.c -shared -lnvrm_graphics -fPIC -o build/nvrm_host1x.so
-$CC nvrm_mem.c -shared -lnvrm -fPIC -o build/nvrm_mem.so
-$CC nvrm_sync.c -shared -lnvrm -fPIC -o build/nvrm_sync.so
+$CC nvrm_host1x.c -shared -lnvrm_graphics -L/usr/lib/aarch64-linux-gnu/tegra -fPIC -o build/nvrm_host1x.so
+$CC nvrm_mem.c -shared -lnvrm -fPIC -L/usr/lib/aarch64-linux-gnu/tegra -o build/nvrm_mem.so
+$CC nvrm_sync.c -shared -lnvrm -fPIC -L/usr/lib/aarch64-linux-gnu/tegra -o build/nvrm_sync.so
