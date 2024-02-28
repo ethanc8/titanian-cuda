dcfd10c24fe69caed159fefd015fa87655939c1c
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 11:20:57 -0600

    Remove the definition of CC

--- nvrm_wrappers/build.sh
+++ nvrm_wrappers/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-CC=clang-14
 $CC nvrm_gpu_titanian.c -shared -fPIC -o build/nvrm_gpu_titanian.so
 $CC nvrm_host1x.c -shared -lnvrm_graphics -fPIC -o build/nvrm_host1x.so
 $CC nvrm_mem.c -shared -lnvrm -fPIC -o build/nvrm_mem.so
