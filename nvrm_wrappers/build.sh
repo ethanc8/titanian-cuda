86848a111634d7ce1a8ab3be34b7647a791777d6
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 12:41:35 -0600

    Fix nvrm_host1x

--- nvrm_wrappers/build.sh
+++ nvrm_wrappers/build.sh
@@ -3,6 +3,6 @@ set -x
 
 mkdir -p build
 gcc nvrm_gpu_titanian.c -shared -fPIC -o build/libnvrm_gpu_titanian.so
-gcc nvrm_host1x.c -shared -Wl,--no-as-needed -lnvrm -lnvrm_graphics -L/usr/lib/aarch64-linux-gnu/tegra -rdynamic -o build/libnvrm_sync.so
+gcc nvrm_host1x.c -shared -Wl,--no-as-needed -lnvrm_graphics -L/usr/lib/aarch64-linux-gnu/tegra -rdynamic -o build/libnvrm_host1x.so
 gcc nvrm_mem.c -shared -Wl,--no-as-needed -lnvrm -L/usr/lib/aarch64-linux-gnu/tegra -rdynamic -o build/libnvrm_mem.so
 gcc nvrm_sync.c -shared -Wl,--no-as-needed -lnvrm -L/usr/lib/aarch64-linux-gnu/tegra -rdynamic -o build/libnvrm_sync.so
