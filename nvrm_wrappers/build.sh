5fab6c09d45d3309a6c5e3b43ce2a1a20412f2bc
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 11:20:20 -0600

    Use clang-14

--- nvrm_wrappers/build.sh
+++ nvrm_wrappers/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-clang nvrm_gpu_titanian.c -shared -fPIC -o build/nvrm_gpu_titanian.so
-clang nvrm_host1x.c -shared -lnvrm_graphics -fPIC -o build/nvrm_host1x.so
-clang nvrm_mem.c -shared -lnvrm -fPIC -o build/nvrm_mem.so
-clang nvrm_sync.c -shared -lnvrm -fPIC -o build/nvrm_sync.so
+CC=clang-14
+$CC nvrm_gpu_titanian.c -shared -fPIC -o build/nvrm_gpu_titanian.so
+$CC nvrm_host1x.c -shared -lnvrm_graphics -fPIC -o build/nvrm_host1x.so
+$CC nvrm_mem.c -shared -lnvrm -fPIC -o build/nvrm_mem.so
+$CC nvrm_sync.c -shared -lnvrm -fPIC -o build/nvrm_sync.so
