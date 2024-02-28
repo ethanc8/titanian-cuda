1a7133b4f74a494ca5e028fffc4c9b8a22204335
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 13:58:40 -0600

    Stub out NvRmMemQueryHeapManager

--- nvrm_wrappers/nvrm_mem.c
+++ nvrm_wrappers/nvrm_mem.c
@@ -1,1 +1,13 @@
-// Just import nvrm
+#include <inttypes.h>
+#include <stdio.h>
+
+typedef enum {
+    NvError_Success = 0,
+    NvSuccess = NvError_Success,
+} NvError;
+
+extern NvError NvRmMemQueryHeapManager(void *thingy) {
+    fprintf(stderr, "nvrm_gpu_titanian: WARNING: NvRmMemQueryHeapManager");
+    fprintf(stderr, "(%p, ...) is not supported\n", thingy);
+    return NvSuccess;
+}
