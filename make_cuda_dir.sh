23ac8e7f44f35e53698e44283e1e42114b6b503a
ethanc8 <echaroenpitaks@gmail.com>
2024-02-28 14:23:49 -0600

    Download nvidia-l4t-3d-core

--- make_cuda_dir.sh
+++ make_cuda_dir.sh
@@ -4,6 +4,8 @@ set -x
 mkdir -p build/cuda-11.4/lib
 wget https://repo.download.nvidia.com/jetson/t194/pool/main/n/nvidia-l4t-cuda/nvidia-l4t-cuda_34.1.1-20220516211757_arm64.deb
 dpkg-deb --raw-extract nvidia-l4t-cuda_34.1.1-20220516211757_arm64.deb build/nvidia-l4t-cuda
+wget https://repo.download.nvidia.com/jetson/t194/pool/main/n/nvidia-l4t-cuda/nvidia-l4t-3d-core_34.1.1-20220516211757_arm64.deb
+dpkg-deb --raw-extract nvidia-l4t-3d-core_34.1.1-20220516211757_arm64.deb build/nvidia-l4t-3d-core
 cp build/nvidia-l4t-cuda/usr/lib/aarch64-linux-gnu/tegra/* build/cuda-11.4/lib
 rm build/cuda-11.4/lib/libcuda.so
 ln -s libcuda.so.1.1 build/cuda-11.4/lib/libcuda.so
